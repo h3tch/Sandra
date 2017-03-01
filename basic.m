5da98c8c48f9c25273b8055cd07f96d9d34469b8
h3tch <michael.hecher@gmx.net>
2017-03-01 23:10:32 +0100

    update

--- basic.m
+++ basic.m
@@ -19,22 +19,55 @@ function basic
     selectedTSun = TSun(sel);
     selectedTShade = TShade(sel);
     
-    meanSun = cell2mat(cellfun(@(t) nanmean(table2array(t(:,6:end))), ...
+    startColumn = 17;
+    
+    nSun = cell2mat(cellfun(@(t) sum(~isnan(table2array(t(:,startColumn:end)))), ...
         selectedTSun, 'uniform', 0));
-    meanShade = cell2mat(cellfun(@(t) nanmean(table2array(t(:,6:end))), ...
+    nShade = cell2mat(cellfun(@(t) sum(~isnan(table2array(t(:,startColumn:end)))), ...
         selectedTShade, 'uniform', 0));
     
-    stdSun = cell2mat(cellfun(@(t) nanstd(table2array(t(:,6:end))), ...
+    meanSun = cell2mat(cellfun(@(t) nanmean(table2array(t(:,startColumn:end))), ...
         selectedTSun, 'uniform', 0));
-    stdShade = cell2mat(cellfun(@(t) nanstd(table2array(t(:,6:end))), ...
+    meanShade = cell2mat(cellfun(@(t) nanmean(table2array(t(:,startColumn:end))), ...
         selectedTShade, 'uniform', 0));
     
-    httest = cell2mat(cellfun(@(u,h) ttest2(table2array(u(:,6:end)),table2array(h(:,6:end))), ...
+    stdSun = cell2mat(cellfun(@(t) nanstd(table2array(t(:,startColumn:end))), ...
+        selectedTSun, 'uniform', 0));
+    stdShade = cell2mat(cellfun(@(t) nanstd(table2array(t(:,startColumn:end))), ...
+        selectedTShade, 'uniform', 0));
+    
+    httest = cell2mat(cellfun(@(u,h) ttest2(table2array(u(:,startColumn:end)),table2array(h(:,startColumn:end))), ...
         selectedTSun, selectedTShade, 'uniform', 0));
     
     httest = array2table(httest);
-    httest.Properties.VariableNames = T.Properties.VariableNames(6:end);
+    httest.Properties.VariableNames = T.Properties.VariableNames(startColumn:end);
     httest.Properties.RowNames = cellstr(selectedSp);
+    
+    idx = (1:size(meanSun,2)) * 2 - 1;
+    vars = T.Properties.VariableNames(startColumn:end);
+    
+    mu = zeros(size(meanSun) .* [1 2]);
+    mu(:,idx) = meanSun;
+    mu(:,idx + 1) = meanShade;
+    mu = array2table(mu);
+    mu.Properties.VariableNames(idx) = cellfun(@(i) ['mu_sun_',i], vars, 'uniform', 0);
+    mu.Properties.VariableNames(idx + 1) = cellfun(@(i) ['mu_shade_',i], vars, 'uniform', 0);
+    
+    sd = zeros(size(stdSun) .* [1 2]);
+    sd(:,idx) = stdSun;
+    sd(:,idx + 1) = stdShade;
+    sd = array2table(sd);
+    sd.Properties.VariableNames(idx) = cellfun(@(i) ['sd_sun_',i], vars, 'uniform', 0);
+    sd.Properties.VariableNames(idx + 1) = cellfun(@(i) ['sd_shade_',i], vars, 'uniform', 0);
+    
+    num = zeros(size(stdSun) .* [1 2]);
+    num(:,idx) = nSun;
+    num(:,idx + 1) = nShade;
+    num = array2table(num);
+    num.Properties.VariableNames(idx) = cellfun(@(i) ['num_sun_',i], vars, 'uniform', 0);
+    num.Properties.VariableNames(idx + 1) = cellfun(@(i) ['num_shade_',i], vars, 'uniform', 0);
+    
+    summary = [mu, sd, num];
     
 end
 
